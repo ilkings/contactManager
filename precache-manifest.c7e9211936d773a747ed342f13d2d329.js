cef8d3059712782106f493f88cebb02f904262d8
Administrator <hihi19931010@outlook.com>
2018-11-06 18:30:21 +0800

    Updates

---
+++ precache-manifest.c7e9211936d773a747ed342f13d2d329.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "40a9e0bf3dbd6723fef7",
+    "url": "/test/static/css/main.26e1c64b.chunk.css"
+  },
+  {
+    "revision": "40a9e0bf3dbd6723fef7",
+    "url": "/test/static/js/main.40a9e0bf.chunk.js"
+  },
+  {
+    "revision": "caf01fbe09553cab4264",
+    "url": "/test/static/css/1.62ec7792.chunk.css"
+  },
+  {
+    "revision": "caf01fbe09553cab4264",
+    "url": "/test/static/js/1.caf01fbe.chunk.js"
+  },
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "ec0d7ac1ed1cda474ec9e7fe15cf161f",
+    "url": "/test/index.html"
+  }
+];
